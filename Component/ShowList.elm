2920d2f84668468cb08b0743e53fc2495e5b255d
Greg Ziegan <greg.ziegan@gmail.com>
2015-09-07 15:10:54 -0400

    can review shows with scores as well

--- Component/ShowList.elm
+++ Component/ShowList.elm
@@ -6,14 +6,13 @@ import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 import Json.Decode as Json
 
-
 -- MODEL
 
 type alias Model =
   {
       shows : List ( ID, Show.Model ),
       nextID : ID,
-      newShowTitle : String
+      newShow : Show.Model
   }
 
 type alias ID = Int
@@ -23,7 +22,7 @@ init =
   {
       shows = [],
       nextID = 0,
-      newShowTitle = ""
+      newShow = Show.init "" 0
   }
 
 -- UPDATE
@@ -31,6 +30,7 @@ init =
 type Action
     = Add
     | UpdateNewShowTitle String
+    | UpdateNewShowScore Show.Score
     | Remove ID
     | Modify ID Show.Action
 
@@ -39,13 +39,18 @@ update action model =
   case action of
     Add ->
       { model |
-          shows <- ( model.nextID, Show.init model.newShowTitle 5) :: model.shows,
+          shows <- ( model.nextID, Show.init model.newShow.title model.newShow.score) :: model.shows,
           nextID <- model.nextID + 1
       }
 
     UpdateNewShowTitle title ->
       { model |
-          newShowTitle <- title
+          newShow <- Show.init title model.newShow.score
+      }
+
+    UpdateNewShowScore score ->
+      { model |
+          newShow <- Show.init model.newShow.title score
       }
 
     Remove id ->
@@ -67,20 +72,32 @@ update action model =
 
 view : Signal.Address Action -> Model -> Html
 view address model =
-  div [] (showEntry address model.newShowTitle :: List.map (viewShow address) model.shows)
+  let reviewShow =
+      showEntry address model.newShow
+  in
+      div [] (reviewShow :: List.map (viewShow address) model.shows)
 
-showEntry : Signal.Address Action -> String -> Html
-showEntry address title =
-  input [
-          id "new-show",
-          placeholder "New Show",
-          autofocus True,
-          value title,
-          name "newShow",
-          on "input" targetValue (Signal.message address << UpdateNewShowTitle),
-          onEnter address Add
-        ]
-        []
+showEntry : Signal.Address Action -> Show.Model -> Html
+showEntry address show =
+  div [] [
+    input [
+            id "new-show-title",
+            placeholder "New Show Title",
+            autofocus True,
+            value show.title,
+            name "newShowTitle",
+            on "input" targetValue (Signal.message address << UpdateNewShowTitle)
+          ]
+          [],
+    br [] [],
+    button [ onClick address (UpdateNewShowScore 1) ] [ text "1" ],
+    button [ onClick address (UpdateNewShowScore 2) ] [ text "2" ],
+    button [ onClick address (UpdateNewShowScore 3) ] [ text "3" ],
+    button [ onClick address (UpdateNewShowScore 4) ] [ text "4" ],
+    button [ onClick address (UpdateNewShowScore 5) ] [ text "5" ],
+    br [] [],
+    button [ onClick address Add ] [ text "Review" ]
+  ]
 
 viewShow : Signal.Address Action -> (ID, Show.Model) -> Html
 viewShow address (id, model) =
