031da171fb1415b3038bb9d83ab7ee1eab26158a
Greg Ziegan <greg.ziegan@gmail.com>
2015-09-30 18:00:49 -0400

    add more mock assets

--- Playground.elm
+++ Playground.elm
@@ -5,13 +5,20 @@ import Svg exposing (Svg)
 main : Signal Svg.Svg
 main =
   let
-     fireflyPath = "./mock/show/firefly/"
+     showPath = "./mock/show/"
+     fireflyPath = showPath ++ "firefly/"
      fireflyBanner = fireflyPath ++ "firefly-banner.jpg"
      fireflyPoster = fireflyPath ++ "firefly-poster.jpg"
+     buffyPath = showPath ++ "buffy/"
+     buffyBanner = buffyPath ++ "buffy-banner.jpg"
+     buffyPoster = buffyPath ++ "buffy-poster.jpg"
+     dollhousePath = showPath ++ "dollhouse/"
+     dollhouseBanner = dollhousePath ++ "dollhouse-banner.jpg"
+     dollhousePoster = dollhousePath ++ "dollhouse-poster.jpg"
   in
     start
       {
-        model = init "Firefly" fireflyBanner fireflyPoster,
+        model = init "Buffy" buffyBanner buffyPoster,
         update = update,
         view = view
       }
