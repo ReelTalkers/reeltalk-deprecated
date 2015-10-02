1f80c2ea0e54d67d5749729d35dcd731d6e8221f
Greg Ziegan <greg.ziegan@gmail.com>
2015-10-01 21:01:58 -0400

    reformatted Review; added ShowFilter

--- Component/Review.elm
+++ Component/Review.elm
@@ -13,24 +13,24 @@ import Html.Events exposing (..)
 
 
 type alias Model =
-    {
-        show : Show.Model,
-        score : Score,
-        createdAt : Time,
-        user : User.Model
-    }
+  {
+    show : Show.Model,
+    score : Score,
+    createdAt : Time,
+    user : User.Model
+  }
 
 type alias Score = Int
 
 
 init : Show.Model -> Score -> Time -> User.Model -> Model
 init show score createdAt user =
-    {
-        show = show,
-        score = score,
-        createdAt = createdAt,
-        user = user
-    }
+  {
+    show = show,
+    score = score,
+    createdAt = createdAt,
+    user = user
+  }
 
 
 -- UPDATE
@@ -40,11 +40,11 @@ type Action = UpdateScore Score
 
 update : Action -> Model -> Model
 update action model =
-    case action of
-        UpdateScore newScore ->
-            { model |
-                score <- newScore
-            }
+  case action of
+    UpdateScore newScore ->
+      { model |
+          score <- newScore
+      }
 
 
 -- VIEW
@@ -52,50 +52,50 @@ update action model =
 
 view: Signal.Address Action -> Model -> Html
 view address model =
-    div []
+  div []
     [
-        h2 [] [text model.show.title],
-        br [] [],
-        button [ onClick address (UpdateScore 1) ] [ text "1" ],
-        button [ onClick address (UpdateScore 2) ] [ text "2" ],
-        button [ onClick address (UpdateScore 3) ] [ text "3" ],
-        button [ onClick address (UpdateScore 4) ] [ text "4" ],
-        button [ onClick address (UpdateScore 5) ] [ text "5" ],
-        br [] [],
-        span [] [text ("Reviewed At: " ++ (currentTime model.createdAt))],
-        br [] [],
-        span [] [text ("Reviewed By: " ++ (model.user.handle))]
+      h2 [] [text model.show.title],
+      br [] [],
+      button [ onClick address (UpdateScore 1) ] [ text "1" ],
+      button [ onClick address (UpdateScore 2) ] [ text "2" ],
+      button [ onClick address (UpdateScore 3) ] [ text "3" ],
+      button [ onClick address (UpdateScore 4) ] [ text "4" ],
+      button [ onClick address (UpdateScore 5) ] [ text "5" ],
+      br [] [],
+      span [] [text ("Reviewed At: " ++ (currentTime model.createdAt))],
+      br [] [],
+      span [] [text ("Reviewed By: " ++ (model.user.handle))]
     ]
 
 
 currentTime : Time -> String
 currentTime t =
   let date' = fromTime t
-      hour' = toString (Date.hour date')
-      minute' = toString (Date.minute date')
-      second' = toString (Date.second date')
-      year' = toString (year date')
-      now = hour' ++ ":" ++ minute' ++ ":" ++ second'
+    hour' = toString (Date.hour date')
+    minute' = toString (Date.minute date')
+    second' = toString (Date.second date')
+    year' = toString (year date')
+    now = hour' ++ ":" ++ minute' ++ ":" ++ second'
   in 
-      now
+    now
 
 type alias Context =
-    {
-      actions : Signal.Address Action,
-      remove : Signal.Address ()
-    }
+  {
+    actions : Signal.Address Action,
+    remove : Signal.Address ()
+  }
 
 viewWithRemoveButton : Context -> Model -> Html
 viewWithRemoveButton context model =
-    div []
+  div []
     [
-        h2 [] [text model.show.title],
-        br [] [],
-        button [ onClick context.actions (UpdateScore 1) ] [ text "1" ],
-        button [ onClick context.actions (UpdateScore 2) ] [ text "2" ],
-        button [ onClick context.actions (UpdateScore 3) ] [ text "3" ],
-        button [ onClick context.actions (UpdateScore 4) ] [ text "4" ],
-        button [ onClick context.actions (UpdateScore 5) ] [ text "5" ],
-        br [] [],
-        button [ onClick context.remove () ] [ text "X" ]
+      h2 [] [text model.show.title],
+      br [] [],
+      button [ onClick context.actions (UpdateScore 1) ] [ text "1" ],
+      button [ onClick context.actions (UpdateScore 2) ] [ text "2" ],
+      button [ onClick context.actions (UpdateScore 3) ] [ text "3" ],
+      button [ onClick context.actions (UpdateScore 4) ] [ text "4" ],
+      button [ onClick context.actions (UpdateScore 5) ] [ text "5" ],
+      br [] [],
+      button [ onClick context.remove () ] [ text "X" ]
     ]
---
+++ Component/ShowFilter.elm
@@ -0,0 +1,74 @@
+module Component.ShowFilter where
+
+import Component.ShowList as ShowList
+import Component.Group as Group
+import Component.User as User
+import Html exposing (..)
+
+-- MODEL
+
+type alias Model =
+  {
+    shows : ShowList.Model,
+    groups : List Group.Model,
+    users: List User.Model
+  }
+
+
+init : Model
+init =
+  {
+    shows = ShowList.init,  -- eventually entire show list
+    groups = [],
+    users = []
+  }
+
+initWithGroups : List Group.Model -> Model
+initWithGroups groups =
+  {
+    shows = ShowList.init,
+    groups = groups,
+    users = []
+  }
+
+initWithUsers : List User.Model -> Model
+initWithUsers users =
+  {
+    shows = ShowList.init,
+    groups = [],
+    users = users
+  }
+
+initWithGroupsAndUsers : List Group.Model -> List User.Model -> Model
+initWithGroupsAndUsers groups users =
+  {
+    shows = ShowList.init,
+    groups = groups,
+    users = users
+  }
+
+-- UPDATE
+
+type Action
+  = FilterShows
+
+update : Action -> Model -> Model
+update action model =
+  case action of
+    FilterShows ->
+      { model |
+          shows <- filterShows model.groups model.users model.shows 
+      }
+
+filterShows : List Group.Model -> List User.Model -> ShowList.Model -> ShowList.Model
+filterShows groups users showList =
+  showList
+
+-- VIEW
+
+view : Signal.Address Action -> Model -> Html
+view address model =
+  div []
+    [
+      p [] [text "filter"]
+    ]
